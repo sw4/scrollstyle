8e07e1632260bd3f2026b315a4d6c160e0e3c237
SW4 <samuel.wheeler@gmail.com>
2014-03-27 15:51:19 +0000

    update 0.0.1

---
+++ scrollstyle.js
@@ -0,0 +1,13 @@
+ $( document ).ready(function() {
+    var scrollCSS= function(){
+        $('.ss-scroll-trigger').not('.ss-activated').each(function(index){
+            var el=$(this);
+            var offset=el.offset().top- $(window).scrollTop();
+            if(offset<$(window).height()*.9){
+                el.removeClass('.ss-scroll-trigger').addClass('ss-activated');
+            }            
+        });
+    };
+    scrollCSS();
+    $(window).scroll(scrollCSS);
+});
